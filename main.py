12cd5bc56501a3a720f439b0a567847c24785508
oriky24 <0riturgenan2@gmail.com>
2025-01-08 18:02:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import pandas as pd
+import joblib
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
 df = pd.read_csv('parkinsons.csv')
 df = df.dropna()
-df.head(10)
-import seaborn as sns
-sns.pairplot(df, hue='status')
-X = df[['Shimmer:APQ3', 'MDVP:Fo(Hz)' ]]
+
+features = ["Shimmer:APQ3", "MDVP:Fo(Hz)"]
+X = df[features]
 y = df['status']
-from sklearn.preprocessing import MinMaxScaler
+
 scaler = MinMaxScaler()
 X_scaled = scaler.fit_transform(X)
-X_scaled
-from sklearn.model_selection import train_test_split
+
 X_train, X_val, y_train, y_val = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
-train_test_split(X_scaled, y, test_size=0.2, random_state=42
-from sklearn.neighbors import KNeighborsClassifier
+
 knn_model = KNeighborsClassifier(n_neighbors=10)
 knn_model.fit(X_train, y_train)
-from sklearn.metrics import accuracy_score
 
 y_pred = knn_model.predict(X_val)
 accuracy = accuracy_score(y_val, y_pred)
-
 print(f"Accuracy: {accuracy}")
 
-if accuracy < 0.8:
-  print("Accuracy is below the threshold of 0.8. Try adjusting the model parameters or features.")
-                 
+joblib.dump(knn_model, 'model.joblib')
+joblib.dump(scaler, 'scaler.joblib')
